d378bedeb374d64180b467eb1058890bee30d8fc
wilkinson <simon_wilkinson92@hotmail.com>
2014-12-15 15:22:19 +0000

    Setup script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+ln -s ~/Documents/TestTemplate/test.h .
+ln -s ~/Documents/TestTemplate/test.cpp .
+ln -s ~/Documents/TestTemplate/program.py .
+ln -s ~/Documents/TestTemplate/parameter* .
+ln -s ~/Documents/TestTemplate/generate.py .
